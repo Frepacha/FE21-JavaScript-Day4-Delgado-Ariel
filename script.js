8b9535a6a05bbc055be03961eaca7ad3330dcd84
Freddy <freddy.delgado.porras@gmail.com>
2021-03-09 14:33:59 +0100

    we did the basic 1 an 2

--- script.js
+++ script.js
@@ -2,23 +2,37 @@
 
 If the length of your name is larger than 5 characters, change the text colour to green. Otherwise, change the text-colour to red. */
 
-function Firstname() {
-    var value = document.getElementsById("FirstName").value;
+function Name() {
+    var value = document.getElementById("FirstName").value;
     var value1 = document.getElementById("LastName").value;
-    var value2 = document.getElementsById("Age").value;
+    var value2 = document.getElementById("Age").value;
+    var value4 = document.getElementById("select").value;
 
-    let Firstname = "";
+    let Firstname = " ";
     if (value > 5) {
-        Firstname = `<p class="green">${value}</p>`
+        Firstname = `<div class="green">${value}</div>`
+    } else { Firstname = `<div class="red">${value}</div>` }
+
+
+    let LastName = " ";
+    if (value1 > 5) {
+        LastName = `<div class="green">${value1}</div>`
+    } else { LastName = `<div class="red">${value1}</div>` }
+
+    let Age = " ";
+    if (value2 > 18) {
+        Age = `<div class="green">${value2}</div>`
+    } else { Age = `<div class="red">${value2}</div>` }
+
+    let select = " ";
+    if (value4 == "IT") {
+        select = `<div class="purple">${value4}</div>`
+    } else {
+        select = `<div class="yellow">${value4}</div>`
     }
-    else { Firstname = `<p class="red">${value}</p>` }
 
+    //console.log(value)
+    document.getElementById("list").innerHTML += `${Firstname} ${LastName} ${Age} ${select}`;
 
-    let LastName = "";
-    if (value1 > )
-
-
-        console.log(value)
-    document.getElementById("list").innerHTML += `<div>${value}</div>`;
 }
-document.getElementsById("btn").addEventListener("click", result);
+document.getElementById("btn").addEventListener("click", Name);
